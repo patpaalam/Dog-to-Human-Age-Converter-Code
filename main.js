28d35cf75a2f2b453605b81b14f8151eb59e53fa
Pat Paalam <106567198+patpaalam@users.noreply.github.com>
2022-07-09 00:34:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // humanAge = (dogAge - 2) * 4 + 21
 
+function dogMain() {
+    for(let i = 0; i < 100; i++)
 let dogAge = prompt("How old is your dog?");
 
 function humanAge() {
@@ -8,6 +10,9 @@ function humanAge() {
 };
 
 alert(` Your dog is ${humanAge(dogAge)} in human age.`);
+};
+
+dogMain()
 // create a function that converts dog age to human time
 // user inputs dogAge. 
 // dogAge - 2 
